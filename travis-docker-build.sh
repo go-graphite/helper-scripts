71b4913095bfc83398481a722d0ade69b826dadd
Vladimir Smirnov <civiloid@google.com>
2019-06-29 17:13:16 +0200

    Remove debian:jessie from list of OSes

--- travis-docker-build.sh
+++ travis-docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # centos:6 
-OS="centos:6 centos:7 ubuntu:16.04 ubuntu:18.04 debian:jessie debian:stretch debian:buster"
+OS="centos:6 centos:7 ubuntu:16.04 ubuntu:18.04 debian:stretch debian:buster"
 if [[ "${DRY_RUN}" == "true" ]]; then
     OS="ubuntu:18.04"
 fi
