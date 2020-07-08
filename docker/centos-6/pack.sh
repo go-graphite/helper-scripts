130c96b58e4acd25cf80344affa22be37e778571
Vladimir Smirnov <civiloid@google.com>
2020-07-08 23:33:53 +0200

    Update Dockerfiles and packaging scripts for build images
    This is only used in carbonapi's travis and shouldn't be used in production in any way.

--- docker/centos-6/pack.sh
+++ docker/centos-6/pack.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+TYPE="${1}"
 export PATH="/root/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${PATH}"
 export GOPATH="/root/go"
 [[ -f /etc/extra_opts ]] && . /etc/extra_opts
@@ -13,36 +14,32 @@ if [[ "${DISTRO}" == "centos" ]]; then
         DISTRO="el"
         VERSION=$(cut -d'.' -f 1 <<< ${LSB_R})
         PKG="rpm"
-	if [[ "${VERSION}" -eq 6 ]]; then
-		. /etc/profile.d/ruby.sh
-		PATH="${PATH}:/opt/rh/ruby193/root/usr/local/bin/:/opt/rh/ruby193/root/usr/bin/"
-	fi
 else
-	lsb_release -i -s | grep -q "Ubuntu"
-	if [[ $? -eq 0 ]]; then
-		VERSION="${LSB_R}"
-	else
-		VERSION="${LSB_C}"
-	fi
+        lsb_release -i -s | grep -q "Ubuntu"
+        if [[ $? -eq 0 ]]; then
+                VERSION="${LSB_R}"
+        else
+                VERSION="${LSB_C}"
+        fi
 fi
 
 pushd ~/go/src/github.com/go-graphite/${1}/
 make clean
-git describe --abbrev=6 --dirty --always --tags | grep -q '\-'
-if [[ $? -ne 0 ]]; then
+REPOS="autobuilds"
+
+git describe --abbrev=6 --dirty --always --tags | grep -q '^[0-9]\+\.[0-9]\+\(\.[0-9]\+\)\?$'
+if [[ $? -eq 0 ]]; then
         REPOS="stable autobuilds"
 fi
-REPOS="autobuilds"
 git reset --hard
-#git pull
 rm -f *.rpm
 rm -f *.deb
 set -e
-/bin/bash +x ./contrib/${1}/fpm/create_package_${PKG}.sh
+/bin/bash -x /root/create_package_${PKG}.sh
 set +e
 
 for r in ${REPOS}; do
-	mkdir -p /root/pkg/${DISTRO}/${VERSION}/${r}
-	cp ./*.${PKG} /root/pkg/${DISTRO}/${VERSION}/${r}/
+        mkdir -p /root/pkg/${DISTRO}/${VERSION}/${r}
+        cp /root/*.${PKG} /root/pkg/${DISTRO}/${VERSION}/${r}/
 done
 popd
--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -e
-#OS="centos:7"
-#OS="debian:jessie debian:stretch debian:buster"
-OS="centos:6 centos:7 ubuntu:14.04 ubuntu:16.04 debian:jessie debian:stretch debian:buster"
+OS="ubuntu:16.04 ubuntu:18.04 ubuntu:20.04 debian:stretch debian:buster centos:8 centos:7 centos:6"
 SQUASH=false
 for i in ${OS}; do
 	folder=$(sed 's/:/-/g;s/\.//g' <<< ${i})
 	echo "Building for $folder"
 	if [[ "${SQUASH}" == "true" ]]; then
 		docker build -t civilus/gographite-build-tmp-${i} ./${folder}/
-		ID=$(docker image inspect civilus/gographite-build-tmp-centos:6 | jq '.[0]["Id"]' | cut -d: -f 2 | cut -d'"' -f 1)
+		ID=$(docker image inspect civilus/gographite-build-tmp-${i} | jq '.[0]["Id"]' | cut -d: -f 2 | cut -d'"' -f 1)
 		docker save ${ID} | /root/go/bin/docker-squash -t civilus/gographite-build-${i} -verbose | docker load
 	else
 		docker build -t civilus/gographite-build-${i} ./${folder}/
